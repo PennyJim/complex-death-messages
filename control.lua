73b20684ef845388c3986863e6924d185a6a5e4e
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-03-12 19:56:34 -0600

    Initial commit

---
+++ control.lua
@@ -0,0 +1,86 @@
+---Disables Better Chat's handler and registers our own
+---@param event defines.events
+---@param func fun(EventData)
+---@param filter EventFilter?
+function replace_event(event, func, filter)
+	remote.call("better-chat", "disable_listener", script.mod_name, defines.events.on_player_died)
+	script.on_event(event, func, filter)
+end
+
+function send_message(message, color, send_level, recipient)
+	remote.call("better-chat", "send", message, color, send_level, recipient)
+end
+
+
+script.on_event(defines.events.on_entity_damaged, function (event)
+	global.lastDamage = global.lastDamage or {} --[[@as table<integer,{damage: LuaDamagePrototype, force: LuaForce?}>]]
+	if not event.entity.name == "character" then return log("Filter is bad!, got "..event.entity.type) end
+	global.lastDamage[event.entity.player.index] = {
+		damage = event.damage_type,
+		force = event.force,
+	}
+end, {
+	{
+		filter = "type",
+		type = "character",
+	},
+	{
+		filter = "final-health",
+		comparison = "<=",
+		value = 0,
+		mode = "and"
+	}
+})
+
+local newDeathListener = function (event)
+	local player = game.get_player(event.player_index)
+	if not player then return log("No one died???") end
+	if not player.character then return log("Player.character doesn't exist on death, change to pre-death") end
+	local message = {
+		"multiplayer.player-died",
+		player.name,
+		player.character.gps_tag --[[@as LocalisedString]]
+	}
+	if event.cause then
+		local cause_name = event.cause.localised_name
+		if event.cause.name  == "character" and event.cause.player then
+			cause_name = event.cause.player.name
+		elseif event.cause.type == "car" or event.cause.type == "spider-vehicle" then
+			local vehicle = event.cause --[[@as LuaEntity]]
+			local last_damage = global.lastDamage[event.player_index]
+			local driver = vehicle.get_driver() or {}
+			local gunner = vehicle.get_passenger() or {}
+			if driver.player then driver = driver.player end
+			if gunner.player then gunner = gunner.player end
+			local old_cause = cause_name
+			if last_damage.damage.name == "impact" then
+		---@diagnostic disable-next-line: need-check-nil
+				cause_name = driver.name
+			else
+			---@diagnostic disable-next-line: need-check-nil
+				cause_name = (vehicle.driver_is_gunner and driver.name) or gunner.name or driver.name
+			end
+		
+			if cause_name then
+				cause_name = {"complex-deaths.bc-by-with", cause_name, old_cause}
+			else
+				cause_name = old_cause
+			end
+		end
+
+		message[1] = "multiplayer.player-died-by"
+		message[4] = message[3]
+		message[3] = cause_name
+	end
+	send_message(message, player.chat_color, "global")
+end
+
+
+script.on_init(function ()
+	global.lastDamage = {} --[[@as table<integer,{damage: LuaDamagePrototype, force: LuaForce?}>]]
+	replace_event(defines.events.on_player_died, newDeathListener)
+end)
+script.on_load(function ()
+	global.lastDamage = global.lastDamage or {}
+	replace_event(defines.events.on_player_died, newDeathListener)
+end)
